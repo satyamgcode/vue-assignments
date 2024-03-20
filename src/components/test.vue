c72681eabf6bebf48484da71f347a69242084509
satyamgcode <guptasatyamcode@gmail.com>
2024-03-20 19:00:02 +0530

    form takes user inputs  and only five user inout can be shown at a time and data can be sorted acc. to date of birth. for next and previous details it have previous and next button

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 
 defineProps({
   msg: String,
@@ -10,28 +10,62 @@ const listItemsArray = reactive([])
 const fname = ref('')
 const lname = ref('')
 const address = ref('')
-// const editIndex = ref(-1)
+const DOB = ref('')
+
+
+let pageSize = 5
+const currentPage = ref(0)
 
 
 function addItems() {
-  if (!fname.value || !lname.value || !address.value) {
+  if (!fname.value || !lname.value || !address.value || !DOB.value) {
     return alert('field cannot be empty')
   }
 
-  listItemsArray.unshift({
+  listItemsArray.push({
     fname: fname.value,
     lname: lname.value,
     address: address.value,
-    color: ''
+    DOB: DOB.value,
+    color: '',
   })
 
-  fname.value = lname.value = address.value = ' '
+  fname.value = lname.value = address.value = DOB.value = ''
 }
 
 
 const deleteItem = (index) => {
   listItemsArray.splice(index, 1)
 }
+
+const showNextButton = computed(() => {
+  // Check if there are more items after the current page
+  return (currentPage.value + 1) * pageSize < listItemsArray.length;
+})
+
+const showPreviousButton = computed(() => {
+  // Check if we're not on the first page
+  return currentPage.value > 0;
+})
+
+const nextPage = () => {
+  // Ensure we don't go beyond the last page
+  currentPage.value = Math.min(currentPage.value + 1, Math.ceil(listItemsArray.length / pageSize) - 1);
+}
+
+const previousPage = () => {
+  // Ensure we don't go below the first page
+  currentPage.value = Math.max(currentPage.value - 1, 0);
+}
+
+const displayedItems = computed(() => {
+  const startIndex = currentPage.value * pageSize;
+  // Use listItemsArray directly for calculation
+
+  return listItemsArray
+    .sort((a, b) => new Date(a.DOB) - new Date(b.DOB)) // Sort based on DOB
+    .slice(startIndex, startIndex + pageSize);
+})
 
 </script>
 
@@ -49,6 +83,9 @@ const deleteItem = (index) => {
   <label for="address">address- </label>
   <input style="margin-right: 0.5rem;" id="address" type="text" v-model="address">
 
+  <label for="date">DOB- </label>
+  <input style="margin-right: 0.5rem;" id="date" type="date" v-model="DOB">
+
   <button @click="addItems">ADD</button>
   <div class="inputList">
     <table>
@@ -57,14 +94,17 @@ const deleteItem = (index) => {
           <th>First name</th>
           <th>Last name</th>
           <th>Address</th>
+          <th>DOB</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(user, index) in  listItemsArray " :key="index" :style="{ 'color': user.color }">
+        <tr v-for="(user, index) in  displayedItems " :key="index" :style="{ 'color': user.color }">
           <td>{{ user.fname }}</td>
           <td>{{ user.lname }}</td>
           <td>{{ user.address }}</td>
+          <td>{{ user.DOB }}</td>
+
           <td class="action">
             <button @click="deleteItem(index)">Delete</button>
             <select name="colors" id="color" class="select" v-model="user.color">
@@ -75,8 +115,16 @@ const deleteItem = (index) => {
             </select>
           </td>
         </tr>
+
       </tbody>
     </table>
+    <div>
+      <button @click="previousPage" :disabled="!showPreviousButton">
+        Previous
+      </button>
+      <span>Page {{ currentPage + 1 }}</span>
+      <button @click="nextPage" :disabled="!showNextButton">Next</button>
+    </div>
   </div>
 </template>
 
