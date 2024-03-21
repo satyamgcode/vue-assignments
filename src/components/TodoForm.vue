9ccfd9b83e79e77c2c0454b8c50d228055d9520d
satyamgcode <guptasatyamcode@gmail.com>
2024-03-21 19:34:27 +0530

    to do list using Pinia have delete button also

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <form @submit.prevent="addItemAndClear">
+            <input v-model="todo" type="text" />
+            <button class="btnInput" style="margin-left: 1rem; margin-top: 4rem;">Add</button>
+            <div class="alert-div">
+                <div v-show="store.showAlert" class="alert">
+                    Please type something...
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+import { useTodoListStore } from '../store/useTodoListStore';
+
+const todo = ref("");
+const store = useTodoListStore();
+
+function addItemAndClear() {
+    if (todo.value.length === 0) {
+        store.inputAlert();
+        return;
+    }
+    store.addTodo(todo.value);
+    todo.value = "";
+}
+
+</script>
+
+<style scoped>
+input {
+    height: 3rem;
+    width: 20rem;
+    border: none;
+    border-radius: 1rem;
+    padding: 0.5rem;
+    padding-left: 1rem;
+    font-size: 1.1rem;
+    margin-bottom: 2rem;
+}
+
+.btnInput {
+    height: 3rem;
+    width: 6rem;
+    color: white;
+    background-color: rgb(72, 68, 68);
+    border: none;
+    border-radius: 2rem;
+    font-size: 1rem;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-for="todo in todoList" :key="todo.id" class="list">
+        <div class="item">
+            <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+            <div>
+                <button @click="deleteTodo(todo.id)" class="x">&#10060;</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useTodoListStore } from "../store/useTodoListStore";
+import { storeToRefs } from "pinia";
+
+const store = useTodoListStore();
+const { todoList } = storeToRefs(store);
+const { deleteTodo } = store;
+
+</script>
+
+<style scoped>
+.item {
+    display: flex;
+    margin-top: 0.5rem;
+    justify-content: center;
+}
+
+div span {
+    min-width: 8rem;
+    font-size: 1.5rem;
+}
+
+.list {
+    text-align: center;
+}
+
+button {
+    border: none;
+    background-color: none;
+    height: 2rem;
+    width: 2rem;
+    border-radius: 1rem;
+}
+</style>
